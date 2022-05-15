d6131fa72f1bde069ecda6a2d1a4c0a1e3162968
violetta31 <violett.kiseleva@yandex.ru>
2022-05-15 17:44:23 +0300

    js

---
+++ js base/calcvuejs/src/App.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div id="app">
+    <CalculatorAction/>
+  </div>
+</template>
+
+<script>
+import CalculatorAction from "@/components/CalculatorAction";
+export default {
+  name: 'App',
+  components: {
+    CalculatorAction,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ js base/calcvuejs/src/components/CalculatorAction.vue
@@ -0,0 +1,152 @@
+<template>
+<div id="calculator">
+  <div id="result">
+    <span>{{ valueDisplayed }}</span>
+  </div>
+  <div id="pad">
+    <div class="btn" id="clear" @click="clear">C</div>
+    <div class="btn" @click="handleOp('/')">/</div>
+    <div class="btn" @click="handleDigit(7)">7</div>
+    <div class="btn" @click="handleDigit(8)">8</div>
+    <div class="btn" @click="handleDigit(9)">9</div>
+    <div class="btn" @click="handleOp('x')">x</div>
+    <div class="btn" @click="handleDigit(4)">4</div>
+    <div class="btn" @click="handleDigit(5)">5</div>
+    <div class="btn" @click="handleDigit(6)">6</div>
+    <div class="btn" @click="handleOp('-')">-</div>
+    <div class="btn" @click="handleDigit(1)">1</div>
+    <div class="btn" @click="handleDigit(2)">2</div>
+    <div class="btn" @click="handleDigit(3)">3</div>
+    <div class="btn" @click="handleOp('+')">+</div>
+    <div id="zero" class="btn" @click="handleDigit(0)">0</div>
+    <div id="equal" class="btn" @click="handleOp('=')">=</div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "CalculatorActions",
+  data () {
+    return {
+      currentValue: 0,
+      savedValue: false,
+      currentOp: false
+    }
+  },
+  methods: {
+    clear () {
+      this.currentValue = 0
+      this.savedValue = false
+      this.currentOp = false
+    },
+    handleDigit (digit) {
+      if (this.currentOp === '=') {
+        this.savedValue = false
+      }
+      this.currentValue = this.currentValue * 10 + digit
+    },
+    handleOp (op){
+      if (this.currentOp) {
+         this.process()
+      }
+      else {
+        this.savedValue = this.currentValue
+      }
+      this.currentValue = 0
+      this.currentOp = op
+    },
+    process() {
+      if (this.currentOp === '+') {
+        this.savedValue += this.currentValue
+      }
+      else if (this.currentOp === '-') {
+        this.savedValue -= this.currentValue
+      }
+      else if (this.currentOp === 'x') {
+        this.savedValue *= this.currentValue
+      }
+      else if (this.currentOp === '/') {
+        this.savedValue /= this.currentValue
+      }
+      else if (this.currentOp === '=' && this.currentValue) {
+        this.savedValue = this.currentValue
+      }
+      this.currentValue = 0
+      this.currentOp = false
+    }
+  },
+  computed: {
+     valueDisplayed () {
+      return this.savedValue ?
+        this.currentValue ?
+        this.currentValue : this.savedValue
+        : this.currentValue
+    }
+  }
+};
+</script>
+
+<style scoped>
+html {
+  background: #f5eff5;
+  font-family: sans-serif;
+}
+body {
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  height: 100vh;
+}
+#calculator {
+  border: 2px solid #222;
+  border-radius: 4px;
+  max-width: 300px;
+  background: #e6d5e6;
+}
+#result {
+  font-size: 42px;
+  width: 100%;
+  height: 70px;
+  box-sizing: border-box;
+  display: grid;
+  padding: 10px;
+  justify-items: end;
+  align-items: center;
+  border-bottom: 2px solid #222;
+}
+#pad {
+  display: grid;
+  grid-template-columns: repeat(4, minmax(60px, 1fr));
+  grid-gap: 10px;
+  padding: 15px;
+}
+.btn {
+  cursor: pointer;
+  font-size: 30px;
+  display: grid;
+  padding: 15px 10px;
+  justify-items: center;
+  align-items: center;
+  color: #FAFAFA;
+  border: 1px solid #222;
+  border-radius: 4px;
+  background: #b86ab8;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.6);
+   transition: background-color .4s ease 0s;
+}
+.btn:hover{
+ background-color: #570760;
+box-shadow: none;
+}
+#clear {
+  background: #b86ab8;
+  grid-column: 1/4;
+}
+#zero {
+  grid-column: 1/4;
+}
+#equal {
+  background: #00ec8d;
+}
+</style>
